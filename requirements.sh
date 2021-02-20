471985038b1e75604159003e6e28eee6e422acd2
Leo Pham <ngockhanh.5110@gmail.com>
2021-02-20 10:54:10 +0700

    edit bash

--- requirements.sh
+++ requirements.sh
@@ -3,5 +3,12 @@ mkdir -p data
 wget 'https://github.com/ThanhChinhBK/vietnews/archive/master.zip' -O ./data/vietnews.zip
 unzip './data/vietnews.zip' && rm './data/vietnews.zip'
 
+mv './vietnews-master/data/test_tokenized' './data/test_tokenized'
+mv './vietnews-master/data/train_tokenized' './data/train_tokenized'
+mv './vietnews-master/data/val_tokenized' './data/val_tokenized'
+rm -r './vietnews-master'
+
+mkdir -p training
+
 # Install the requirements.txt
 pip install -r requirements.txt
